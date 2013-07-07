9dd0a5fd61f2fbb234998ba58c5f2587f16a290b
Angus Chen <semistone@gmail.com>
2013-07-07 04:36:35 +0000

    move config to example

--- src/example/config.js
+++ src/example/config.js
@@ -1,5 +1,18 @@
 config = {
+    type: 'fifo',
     path: './example',
+    consumers: [
+        {index: 1,       
+         consume_msg_callback: function(row, callback){
+             console.log('consume:' + row.ID + " data:" + row.DATA);
+             callback(true);}
+        },
+        {index: 2,       
+         consume_msg_callback: function(row, callback){
+             console.log('consume:' + row.ID + " data:" + row.DATA);
+             callback(true);}
+        }
+    ],
     consume_msg_callback: function(row, callback){
         console.log('consume:' + row.ID + " data:" + row.DATA);
         callback(true);
