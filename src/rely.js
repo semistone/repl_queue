9e172cae8ac30cbe6837770a712840f20450375a
angus_chen <angus_chen@trend.com.tw>
2013-07-08 14:06:41 +0800

    debug

--- src/rely.js
+++ src/rely.js
@@ -1,26 +1,27 @@
 var options,
     url = require('url'),
     http = require('http');
-var i = 0;
+//var i = 0;
 /**
  *
  */
 var do_task =  function(row, callback){
     // for testing 
+    /*
     i++;
     if (i > 10) {
         callback(false);
         return;
     }
-
+    */
     var _options = {
         protocol: options.protocal,
         method: 'POST',
         hostname: options.hostname,
         port: options.port,
-        path: '/'+row.CMD + row.ID
+        path: '/' + row.CMD
     };
-    console.log('rely id :' + row.ID + " data:" + row.DATA + ' to ' + _options.hostname);
+    console.log('rely id :' + row.ID + " data:" + row.DATA + ' to ' + _options.hostname + _options.path);
     var req = http.request(_options, function(res) {
         if (res.statusCode == 200) {
             console.log('http rely success for id:"'+row.ID + '"');
