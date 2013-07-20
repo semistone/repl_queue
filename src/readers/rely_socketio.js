74ff23d7eea18cbebf16e5206af11a7476d44f8e
Angus Chen <semistone@gmail.com>
2013-07-20 03:27:09 +0000

    fix jslint

--- src/readers/rely_socketio.js
+++ src/readers/rely_socketio.js
@@ -4,43 +4,47 @@ var io = require('socket.io-client'),
 /**
  * consumer function
  */
-var consumer_function =  function(row, callback){//{{{
-    if(!this.connect_status){
+var consumer_function =  function (row, callback) {//{{{
+    "use strict";
+    if (!this.connect_status) {
         console.log('disconnected');
-        callback(false); 
+        callback(false);
     }
-    this.socket.emit('write', row, function(err){
+    this.socket.emit('write', row, function (err) {
         callback(!err);
     });
-};//}}}
-
-/**
- * rely through socket io constructor
- */
-var constructor = function(rely_to){//{{{
-    console.log('rely to ' + rely_to);
-    this.socket = io.connect(rely_to);
-    this.socket.on('connect', function () {
-        console.log("rely socket connected"); 
-        this.connect_status = true;
-    });
-    this.socket.on('disconnect', function () {
-        console.log("rely socket disconnected"); 
-        this.connect_status = false;
-    });
-    this.consumer_function = consumer_function;
-    this.kill = kill;
 };//}}}
 
 /**
  * kill 
  *
  */
-var kill = function(){//{{{
-    if(this.socket && this.connect_status){
-       this.socket.disconnect();
-       this.socket = undefined;
+var kill = function () {//{{{
+    "use strict";
+    if (this.socket && this.connect_status) {
+        this.socket.disconnect();
+        this.socket = undefined;
     }
 };//}}}
+
+/**
+ * rely through socket io constructor
+ */
+var constructor = function (rely_to) {//{{{
+    "use strict";
+    console.log('rely to ' + rely_to);
+    this.socket = io.connect(rely_to);
+    this.socket.on('connect', function () {
+        console.log("rely socket connected");
+        this.connect_status = true;
+    });
+    this.socket.on('disconnect', function () {
+        console.log("rely socket disconnected");
+        this.connect_status = false;
+    });
+    this.consumer_function = consumer_function;
+    this.kill = kill;
+};//}}}
+
 
 module.exports = constructor;
