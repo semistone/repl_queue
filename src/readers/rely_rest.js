8b5c1eb562f623f770d2ce2057c44498f6d8f776
Angus Chen <semistone@gmail.com>
2013-07-20 03:20:19 +0000

    fix jslint

--- src/readers/rely_rest.js
+++ src/readers/rely_rest.js
@@ -3,48 +3,51 @@
  *
  */
 var url = require('url'),
-    http = require('http');
-var SOCKET_TIMEOUT = 2000;
+    http = require('http'),
+    SOCKET_TIMEOUT = 2000;
 
 /**
  * do task
  *
  */
-var consumer_function =  function(row, callback){//{{{
-    var req_id = row.REQUEST_ID;
+var consumer_function = function (row, callback) {//{{{
+    "use strict";
+    var req_id = row.REQUEST_ID,
+        options,
+        req;
     if (!req_id) {
-       req_id = row.ID; 
+        req_id = row.ID;
     }
-    var _options = {
+    options = {
         protocol: this.options.protocal,
         method: 'POST',
         agent: this.agent,
         hostname: this.options.hostname,
         port: this.options.port,
-        heads:{
+        heads: {
             'Connection': 'Keep-Alive'
         },
         path: this.options.path + '/' + row.CMD + '/' + req_id
     };
-    console.log('rely id :' + row.ID + " data:" + row.DATA + ' to ' + _options.hostname + _options.path);
-    var req = http.request(_options, function(res) {
-        if (res.statusCode == 200) {
+    console.log('rely id :' + row.ID + " data:" + row.DATA + ' to ' + options.hostname + options.path);
+    req = http.request(options, function (res) {
+        if (res.statusCode === 200) {
             console.log('http rely success for id:' + row.ID);
             callback(true);
         } else {
             console.log('http rely fail');
             callback(false);
         }
-        res.on('data', function(data){
-        }); 
-        res.on('end', function(){}); 
+        res.on('data', function (data) {
+        });
+        res.on('end', function () {});
     });
     req.setSocketKeepAlive(true, 1000);
-    req.setTimeout(SOCKET_TIMEOUT, function(){
+    req.setTimeout(SOCKET_TIMEOUT, function () {
         console.log('http connected and timeout for id:' + row.ID);
         callback(false);
     });
-    req.on('error', function(){
+    req.on('error', function () {
         console.log('request error for id:' + row.ID);
         callback(false);
     });
@@ -55,8 +58,9 @@ var consumer_function =  function(row, callback){//{{{
 /**
  * constructor
  */
-var constructor = function(rely_to){//{{{
-    this.agent = new http.Agent;
+var constructor = function (rely_to) {//{{{
+    "use strict";
+    this.agent = new http.Agent();
     this.agent.maxSockets = 1;
     console.log('rely to ' + rely_to);
     this.options = url.parse(rely_to);
