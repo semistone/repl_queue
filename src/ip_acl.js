edfe8d6f4a6fa16556feee6b5eecec5306af7df1
Angus Chen <semistone@gmail.com>
2013-07-20 03:15:27 +0000

    fix jslint

--- src/ip_acl.js
+++ src/ip_acl.js
@@ -2,13 +2,15 @@
  * limit ip to access writer 
  *
  */
-var _allow_ip;
-var filter = function(req){
-    return req.connection.remoteAddress == _allow_ip
+var allow_ip;
+var filter = function (req) {
+    "use strict";
+    return req.connection.remoteAddress === allow_ip;
 };
 
-var ip_acl = function(allow_ip){
-    _allow_ip = allow_ip;
+var ip_acl = function (a_allow_ip) {
+    "use strict";
+    allow_ip = a_allow_ip;
     return filter;
 };
 
