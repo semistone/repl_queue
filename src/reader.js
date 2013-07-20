f9fe947b369a697f2efad98c2c7df569410c7cf9
Angus Chen <semistone@gmail.com>
2013-07-20 02:42:56 +0000

    fix jslint

--- src/reader.js
+++ src/reader.js
@@ -1,31 +1,33 @@
 var fs = require('fs'),
-    emitter = require('events').EventEmitter,
+    Emitter = require('events').EventEmitter,
     sqlite3 = require('sqlite3').verbose(),
-    db = require('./db.js');
+    DB = require('./db.js'),
     sql = require('./sql.js'),
     fifo = require('./fifo.js'),
-    config = require('./example/config.js');
-
-var DELIMITER = '/',
-    killed = false; // if kill signal fired, then killed = true
+    config = require('./example/config.js'),
+    DELIMITER = '/',
+    killed = false, // if kill signal fired, then killed = true
     index_handlers = {};
 /**
  * loop message from sql
  */
-var get_last_record_and_loop_message = function(index, finish_callback) {//{{{
-    var last_update_rows = 0;
-    var working_queue = [], self = this;
+var get_last_record_and_loop_message = function (index, finish_callback) {//{{{
+    "use strict";
+    var last_update_rows = 0,
+        working_queue = [],
+        loop_message,
+        self = this;
     /**
      * use fifo sub module to consume working queue.
      */
-    this.each_complete_callback = new fifo.each_complete_callback(working_queue, config, index, finish_callback); 
+    this.each_complete_callback = new fifo.each_complete_callback(working_queue, config, index, finish_callback);
 
     /**
      * loop message
      *
      */
-    var loop_message = function(last_record){//{{{
-        self.db.volume.each(sql.SELECT_SQL, [last_record], function(err, row){
+    loop_message = function (last_record) {//{{{
+        self.db.volume.each(sql.SELECT_SQL, [last_record], function (err, row) {
             working_queue.push(row);
             console.log('push id ' + row.ID);
         }, self.each_complete_callback);
@@ -35,15 +37,15 @@ var get_last_record_and_loop_message = function(index, finish_callback) {//{{{
      * get last record and start loop message.
      *
      */
-    this.db.meta.get(sql.SELECT_META_SQL, [index],function(error, row) {
-        tableExists = (row != undefined);
+    this.db.meta.get(sql.SELECT_META_SQL, [index], function (error, row) {
+        var tableExists = (row !== undefined);
         if (!tableExists) {
-            self.db.meta.run(sql.INSERT_META_SQL, [index, 0], function(){
-                console.log("insert meta done");    
+            self.db.meta.run(sql.INSERT_META_SQL, [index, 0], function () {
+                console.log("insert meta done");
                 loop_message(0);
             });
         } else {
-            console.log('last record for ' + index +' is ' + row.LAST_RECORD);
+            console.log('last record for ' + index + ' is ' + row.LAST_RECORD);
             self.last_record = row.LAST_RECORD;
             loop_message(row.LAST_RECORD);
         }
@@ -55,38 +57,40 @@ var get_last_record_and_loop_message = function(index, finish_callback) {//{{{
  * else stop processing.
  *
  */
-var loop_scan_message = function(){//{{{
-    var finish_event_emitter = new emitter(),
-        finish_callback, self = this;
+var loop_scan_message = function () {//{{{
+    "use strict";
+    var finish_event_emitter = new Emitter(),
+        finish_callback,
+        self = this;
 
     if (this.processing) {
         console.log('i am processing');
         return;
     }
     this.processing = true;
-    finish_callback = function(rows, rowID){
+    finish_callback = function (rows, rowID) {
         console.log('finish callback');
         if (killed) { // killed signal fired, stop loop.
             finish_event_emitter.removeAllListeners();
             return;
         }
-        if (rows != 0) {
-            self.get_last_record_and_loop_message(self.index, function(rows){
-                console.log('emit finish event, last loop consume rows '+rows);
-                finish_event_emitter.emit('finish', rows);         
+        if (rows !== 0) {
+            self.get_last_record_and_loop_message(self.index, function (rows) {
+                console.log('emit finish event, last loop consume rows ' + rows);
+                finish_event_emitter.emit('finish', rows);
             });
         } else {
-            if (rowID == undefined){
+            if (rowID === undefined) {
                 rowID = self.last_record;
             }
             console.log('end scan message and stop processing row id is ' + rowID);
             self.processing = false;
-            if (rowID > 30){
+            if (rowID > 30) {
                 console.log('current row id is ' + rowID);
-                self.db.volume.get(sql.CHECK_FINISH_VOLUME,[rowID], function(err, row){
+                self.db.volume.get(sql.CHECK_FINISH_VOLUME, [rowID], function (err, row) {
                     if (row.CNT > 0) {
                         console.log("change to next volume");
-                    }else{
+                    } else {
                         console.log('not match last record yet');
                     }
                 });
@@ -103,15 +107,16 @@ var loop_scan_message = function(){//{{{
  *     get_last_record_and_loop_message
  *
  */
-var index_handler = function(index){//{{{
+var IndexHandler = function (index) {//{{{
+    "use strict";
     var self = this;
-    this.db = new db(config);
+    this.db = new DB(config);
     this.processing = false; // if message loop is processing
     this.index = index;
-    this.db.init_reader(index, function(){
-        if(self.db.is_latest){ // only latest file need to watch
+    this.db.init_reader(index, function () {
+        if (self.db.is_latest) { // only latest file need to watch
             self.watchfile();
-        } else{
+        } else {
             self.loop_scan_message();
         }
         self.binding_signal();
@@ -122,19 +127,20 @@ var index_handler = function(index){//{{{
  * start watch db file  -> loop_scan_message
  *
  */
-var watchfile= function(){//{{{
+var watchfile = function () {//{{{
+    "use strict";
     var self = this;
     console.log("watching " + this.db.volume_file);
     this.loop_scan_message();
-    fs.watchFile(this.db.volume_file, function(curr,prev) {
-        if (curr.mtime == prev.mtime) {
+    fs.watchFile(this.db.volume_file, function (curr, prev) {
+        if (curr.mtime === prev.mtime) {
             console.log("mtime equal");
         } else {
             self.loop_scan_message();
             console.log("mtime not equal");
-        }   
+        }
     });
-}//}}}
+};//}}}
 
 
 
@@ -142,12 +148,13 @@ var watchfile= function(){//{{{
  * kill or stop reading queue. 
  *
  */
-var kill = function(){//{{{
+var kill = function () {//{{{
+    "use strict";
     var self = this;
     console.log('unwatch ' + this.db.volume_file);
     fs.unwatchFile(this.db.volume_file);
     killed = true;
-    fifo.kill(function(){
+    fifo.kill(function () {
         console.log('close volme.db');
         self.db.volume.close();
         console.log('close meta.db');
@@ -159,15 +166,16 @@ var kill = function(){//{{{
  * bind kill signal
  *
  */
-var binding_signal = function(){//{{{
+var binding_signal = function () {//{{{
+    "use strict";
     var self = this;
-    process.on('SIGINT', function(){
-       console.log('fire SIGINT in reader');
-       self.kill();
+    process.on('SIGINT', function () {
+        console.log('fire SIGINT in reader');
+        self.kill();
     });
-    process.on('SIGHUP', function(){
-       console.log('fire SIGHUP in reader');
-       self.kill();
+    process.on('SIGHUP', function () {
+        console.log('fire SIGHUP in reader');
+        self.kill();
     });
 };//}}}
 
@@ -177,8 +185,10 @@ var binding_signal = function(){//{{{
  * main 
  *
  */
-(function(){//{{{
-    index_handler.prototype = {
+(function () {//{{{
+    "use strict";
+    var index;
+    IndexHandler.prototype = {
         'loop_scan_message': loop_scan_message,
         'get_last_record_and_loop_message': get_last_record_and_loop_message,
         'watchfile': watchfile,
@@ -186,7 +196,7 @@ var binding_signal = function(){//{{{
         'kill': kill
     };
 
-    for(var index in config.reader){
-        index_handlers[index] = new index_handler(index);
+    for (index = 0; index < config.reader.length; index += 1) {
+        index_handlers[index] = new IndexHandler(index);
     }
-})();//}}}
+}());//}}}
