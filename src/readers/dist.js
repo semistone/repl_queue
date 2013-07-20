35063d0807ea74ae326225742a275e5ffb71fa18
Angus Chen <semistone@gmail.com>
2013-07-20 03:24:21 +0000

    fix jslint

--- src/readers/dist.js
+++ src/readers/dist.js
@@ -8,28 +8,32 @@ var url = require('url'),
 /**
  *
  */
-var consumer_function =  function(row, callback){//{{{
-    var req_id = row.REQUEST_ID;
+var consumer_function =  function (row, callback) {//{{{
+    "use strict";
+    var req_id = row.REQUEST_ID,
+        insert_callback;
+    insert_callback = function (err) {
+        if (err) {
+            console.log(err);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    };
     if (!req_id) {
-       req_id = row.ID; 
+        req_id = row.ID;
     }
     console.log('save id :' + row.ID + " data:" + row.DATA);
     this.volume.run(sql.INSERT_VOLUME_SQL,
-               [req_id, row.CMD, row.DATA, new Date().getTime()/1000],
-               function(err){
-                   if(err){
-                       console.log(err); 
-                       callback(false);
-                   }else{
-                       callback(true);
-                   }
-               });
+               [req_id, row.CMD, row.DATA, new Date().getTime() / 1000],
+               insert_callback);
 };//}}}
 
 /**
  * dist constructor 
  */
-var constructor = function(dist_to){//{{{
+var constructor = function (dist_to) {//{{{
+    "use strict";
     console.log('save to ' + dist_to);
     this.volume = new sqlite3.cached.Database(dist_to);
     this.volume.run(sql.CREATE_SQL);
