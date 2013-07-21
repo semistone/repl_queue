8b812db81db7c28b9a8e19df0dbed03ee7fe6b81
Angus Chen <semistone@gmail.com>
2013-07-21 03:26:04 +0000

    test fs.watch

--- test/test_watchfile/fs_event.js
+++ test/test_watchfile/fs_event.js
@@ -7,15 +7,30 @@
  *     insert into x values ('test');
  */
 var fs = require('fs');                                                                        
-
+var file1 = './test.sqlite';
+var file2 = './test2.sqlite';
+var current = file1;
+var next = file2;
 console.log("Watching test.sqlite");
 
-fs.watchFile('./test.sqlite', function(curr,prev) {
+fs.watchFile(file1, function(curr,prev) {
     console.log("current mtime: " +curr.mtime);
     console.log("previous mtime: "+prev.mtime);
-    if (curr.mtime == prev.mtime) {
+    if (curr.mtime === prev.mtime) {
         console.log("mtime equal");
     } else {
         console.log("mtime not equal");
     }   
 });
+fs.watch(file1, function(action,name) {
+    console.log("action " +action);
+    console.log("name "+name);
+});
+setInterval(function(){
+    console.log('rename ' + current+ ' to ' +next);
+    fs.rename(current, next, function(){
+        var tmp = current;
+        current = next;
+        next = tmp;        
+    });        
+}, 1000);
