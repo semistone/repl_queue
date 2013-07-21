c41ec23c0615cde7ad29af23f534af2d0f639983
Angus Chen <semistone@gmail.com>
2013-07-21 03:47:07 +0000

    test watch

--- test/test_watchfile/fs_event.js
+++ test/test_watchfile/fs_event.js
@@ -8,24 +8,52 @@
  */
 var fs = require('fs');                                                                        
 var file1 = './test.sqlite';
-var file2 = './test2.sqlite';
+var file2 = './test/test.sqlite';
 var current = file1;
 var next = file2;
+var watch;
 console.log("Watching test.sqlite");
+function do_watch(){
+    fs.watchFile(file1, function(curr,prev) {
+        console.log("current mtime: " +curr.mtime);
+        console.log("previous mtime: "+prev.mtime);
+        if (curr.mtime === prev.mtime) {
+            console.log("mtime equal");
+        } else {
+            console.log("mtime not equal");
+        }   
+    });
+    watch = fs.watch(file1, function(action,name) {
+        console.log("action " +action);
+        console.log("name "+name);
+    });
+}
 
-fs.watchFile(file1, function(curr,prev) {
-    console.log("current mtime: " +curr.mtime);
-    console.log("previous mtime: "+prev.mtime);
-    if (curr.mtime === prev.mtime) {
-        console.log("mtime equal");
-    } else {
-        console.log("mtime not equal");
-    }   
-});
-fs.watch(file1, function(action,name) {
-    console.log("action " +action);
-    console.log("name "+name);
-});
+function do_unwatch(){
+    console.log('unwatch ' +current);
+    fs.unwatchFile(current);
+    watch.close();
+}
+/*
+setTimeout(function(){
+    fs.unwatchFile(current);
+    watch.close();
+}, 3000);
+*/
+setInterval(function(){
+   console.log('looop');     
+},
+1000);
+
+do_watch();
+console.log('rename and unwatch original');
+fs.rename(current, next, function(){
+    setTimeout(function(){
+        do_unwatch();
+    }, 1000);
+});        
+
+/*
 setInterval(function(){
     console.log('rename ' + current+ ' to ' +next);
     fs.rename(current, next, function(){
@@ -34,3 +62,4 @@ setInterval(function(){
         next = tmp;        
     });        
 }, 1000);
+*/
